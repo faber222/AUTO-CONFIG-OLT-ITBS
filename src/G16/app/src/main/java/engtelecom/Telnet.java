828a50a3e23dec5dc2c33c860f75d0f4d994dca9
faber222 <faberbernardo78@gmail.com>
2023-12-20 19:52:41 -0300

    esboço do programa

---
+++ src/G16/app/src/main/java/engtelecom/Telnet.java
@@ -0,0 +1,115 @@
+package main.java.engtelecom;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import javax.swing.JOptionPane;
+
+public class Telnet implements Runnable {
+
+    private Socket socket;
+    private BufferedReader in, stdIn;
+    private PrintWriter out;
+    private Thread thread;
+    private boolean active;
+
+    public Telnet(String host, int port, String username, String password, String[] commands) {
+        try {
+            socket = new Socket(host, port);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(), true);
+
+            // Início da Leitura em tempo real:
+            active = true;
+            thread = new Thread(this);
+            thread.start();
+
+            // Login
+            out.println(username);
+            out.println(password);
+
+            // Executa comandos
+            for (String command : commands) {
+                out.println(command);
+            }
+
+        } catch (UnknownHostException exception) {
+            System.err.println("Host " + host + " desconhecido");
+            System.exit(1);
+        } catch (IOException exception) {
+            System.err.println("Erro na entrada.");
+            System.exit(1);
+        }
+
+        // Caso queria liberar o prompt para digitação.
+        // Executa comandos
+        while (true) {
+            String command = getUserInput();
+            if ("Bye.".equals(command)) {
+                System.exit(0);
+            }
+            out.println(command);
+        }
+        // try {
+
+        // stdIn = new BufferedReader(new InputStreamReader(System.in));
+        // String input;
+
+        // while ((input = stdIn.readLine()) != null || !active) {
+        // if (input == "Bye.") {
+        // System.exit(0);
+        // }
+
+        // out.println(input);
+        // }
+
+        // }
+
+        // catch (IOException exception) {
+        // System.err.println("Erro de entrada na comunicacao.");
+        // System.exit(1);
+        // }
+
+    }
+
+    public void run() {
+        try {
+            String answer;
+            while (active) {
+                while ((answer = in.readLine()) != null) {
+                    if (answer.equals("Bye.")) {
+                        System.exit(0);
+                    }
+                    System.out.println(answer);
+                }
+            }
+        } catch (IOException exception) {
+            System.err.println("Erro de comunicação.");
+            System.exit(1);
+        }
+    }
+
+    private static String getUserInput() {
+        return JOptionPane.showInputDialog("Digite um comando:");
+    }
+
+    public static void main(String args[]) {
+        String host;
+        int port;
+        String username;
+        String password;
+        String[] commands;
+
+        host = "10.1.40.131";
+        port = 23;
+        username = "admin";
+        password = "admin";
+        commands = new String[] { "enable", "sh in br", "sh ip interface" };
+
+        new Telnet(host, port, username, password, commands);
+    }
+}
