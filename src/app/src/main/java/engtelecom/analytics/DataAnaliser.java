065298c5dc33d36c3ff8d46ae4497bbc8ddc5622
faber222 <faberbernardo78@gmail.com>
2024-10-13 21:55:07 -0300

    :construction: add logic for DataAnalytics to oltCutover

---
+++ src/app/src/main/java/engtelecom/analytics/DataAnaliser.java
@@ -0,0 +1,118 @@
+package engtelecom.analytics;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import engtelecom.filters.DataLineFilter;
+import engtelecom.filters.DataRuleFilter;
+import engtelecom.filters.DataVlanFilter;
+
+public class DataAnaliser {
+    private ArrayList<String> vlans;
+    private ArrayList<String> lines;
+    private ArrayList<String> rules;
+
+    private ArrayList<String> data;
+
+    private HashMap<Integer, ArrayList<String>> dataMapRules;
+    private HashMap<Integer, ArrayList<String>> dataMapLines;
+    private HashMap<Integer, ArrayList<String>> dataMapVlans;
+
+    public DataAnaliser() {
+        this.vlans = new ArrayList<>();
+        this.lines = new ArrayList<>();
+        this.rules = new ArrayList<>();
+        this.data = new ArrayList<>();
+    }
+
+    public void start() {
+        DataRuleFilter ruleFilter = new DataRuleFilter();
+        DataLineFilter lineFilter = new DataLineFilter();
+        DataVlanFilter vlanFilter = new DataVlanFilter();
+
+        ruleFilter.start();
+        lineFilter.start();
+        vlanFilter.start();
+        this.dataMapRules = ruleFilter.getDataMap();
+        this.dataMapLines = lineFilter.getDataMap();
+        this.dataMapVlans = vlanFilter.getDataMap();
+
+        dataRuleSorter();
+
+    }
+
+    public ArrayList<String> getData() {
+        return data;
+    }
+
+    public ArrayList<String> dataSorter(int x, int key) {
+        // Inicia um switch com base no valor da variável 'key'
+        switch (key) {
+            // Caso o valor de 'key' seja 0
+            case 0:
+                // Percorre o mapa 'dataMapLines', que mapeia um Integer para uma
+                // ArrayList<String>
+                for (Map.Entry<Integer, ArrayList<String>> entry : this.dataMapLines.entrySet()) {
+                    // Se o valor de 'x' for igual à chave da entrada atual do mapa
+                    if (x == entry.getKey()) {
+                        // Retorna o valor associado a essa chave (ArrayList<String>)
+                        return entry.getValue();
+                    }
+                }
+                break;
+
+            // Caso o valor de 'key' seja 1
+            case 1:
+                // Percorre o mapa 'dataMapVlans', que também mapeia um Integer para uma
+                // ArrayList<String>
+                for (Map.Entry<Integer, ArrayList<String>> entry : this.dataMapVlans.entrySet()) {
+                    // Se o valor de 'x' for igual à chave da entrada atual do mapa
+                    if (x == entry.getKey()) {
+                        // Retorna o valor associado a essa chave (ArrayList<String>)
+                        return entry.getValue();
+                    }
+                }
+                break;
+
+            // Se 'key' não for nem 0 nem 1, o código não faz nada (default case)
+            default:
+                break;
+        }
+
+        // Se nenhuma correspondência for encontrada nos mapas, retorna uma lista vazia
+        return new ArrayList<>();
+    }
+
+    @SuppressWarnings("null")
+    public void dataRuleSorter() {
+
+        this.dataMapRules.forEach((lineKey, valueList) -> {
+            // recebe o retorno do dataSorter se não for nulo
+            this.lines = (lineKey != null) ? dataSorter(lineKey, 0) : null;
+
+            // itera os valores obtidos do rule
+            for (String eachRules : valueList) {
+                String local = null;
+
+                // itera os valores obtidos do line
+                for (String eachLines : this.lines) {
+                    // captura o ultimo item do array, que é a chave da hash vlan
+                    String[] arrayEachVlan = eachLines.split(";");
+                    int vlanKey = Integer.parseInt(arrayEachVlan[arrayEachVlan.length - 1]);
+
+                    // recebe o retorno do dataSorter se não for nulo
+                    this.vlans = (vlanKey != 0) ? dataSorter(vlanKey, 1) : null;
+
+                    // itera os valores obtidos do vlan
+                    for (String eachVlan : this.vlans) {
+                        local = eachRules + ";" + eachLines + ";" + eachVlan;
+
+                        // ITBS-5f72cb27;0/9/1;58;1005;veip;TRUE;null;1;1005;1005
+                    }
+                    this.data.add(local);
+                }
+            }
+        });
+    }
+}
---
+++ src/app/src/main/java/engtelecom/filters/DataVlanFilter.java
@@ -0,0 +1,58 @@
+package engtelecom.filters;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DataVlanFilter {
+    private final HashMap<Integer, ArrayList<String>> dataMap2;
+    private final String path;
+
+    public DataVlanFilter() {
+        this.dataMap2 = new HashMap<>();
+        // this.path = "dadosVlan.txt";
+        this.path = "dados.txt";
+    }
+
+    public void start() {
+        // Hashtable onde a chave é o "line" e o valor é uma lista de strings no formato
+        // "XXXX-FFFFFFFF;0/x/y"
+
+        try (BufferedReader br = new BufferedReader(new FileReader(this.path))) {
+            String line;
+            int aim = 0; // Variável para armazenar o valor do aim
+            while ((line = br.readLine()) != null) {
+                // Regex para capturar o aim no formato 0/x/y
+                final Pattern aimPattern = Pattern.compile("aim\\s+(\\d+)");
+                final Matcher aimMatcher = aimPattern.matcher(line);
+                if (aimMatcher.find()) {
+                    aim = Integer.parseInt(aimMatcher.group(1)); // Captura o aim encontrado
+                }
+
+                // Regex para capturar o string-hex genérico e o valor do line
+                final Pattern hexPattern = Pattern
+                        .compile("translate\\s+old-vlan\\s+(\\d+)\\s+new-vlan\\s+(\\d+)");
+                final Matcher hexMatcher = hexPattern.matcher(line);
+                if (hexMatcher.find()) {
+                    final int oldVlan = Integer.parseInt(hexMatcher.group(1)); // Captura o valor do old-vlan
+                    final int newVlan = Integer.parseInt(hexMatcher.group(2)); // Captura o valor do new-vlan
+
+                    // Adiciona o valor na hashMap, agrupando por chave (line)
+                    final String entry2 = oldVlan + ";" + newVlan;
+                    this.dataMap2.computeIfAbsent(aim, K -> new ArrayList<>()).add(entry2);
+                }
+            }
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public HashMap<Integer, ArrayList<String>> getDataMap() {
+        return dataMap2;
+    }
+
+}
