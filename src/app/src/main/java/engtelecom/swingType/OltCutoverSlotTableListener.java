4f8ab9d17345150c1db72bbe6582d1f61916aa34
faber222 <faberbernardo78@gmail.com>
2025-04-04 00:43:07 -0300

    cadastrado método slot table

---
+++ src/app/src/main/java/engtelecom/swingType/OltCutoverSlotTableListener.java
@@ -0,0 +1,11 @@
+package engtelecom.swingType;
+
+import java.util.List;
+
+/**
+ *
+ * @author faber222
+ */
+public interface OltCutoverSlotTableListener {
+    void onProfileCreatedSlotTable(List<String[]> slotSelecionada);
+}
