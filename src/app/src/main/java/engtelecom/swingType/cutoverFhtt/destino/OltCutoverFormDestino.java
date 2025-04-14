5bf1231590a95f1b69bbab4a026454eaa6aae357
faber222 <faberbernardo78@gmail.com>
2025-04-13 23:14:25 -0300

    removendo comentário

--- src/app/src/main/java/engtelecom/swingType/cutoverFhtt/destino/OltCutoverFormDestino.java
+++ src/app/src/main/java/engtelecom/swingType/cutoverFhtt/destino/OltCutoverFormDestino.java
@@ -16,50 +16,6 @@ import java.util.Map;
  */
 public class OltCutoverFormDestino extends javax.swing.JFrame {
 
-    // /**
-    // * @param args the command line arguments
-    // */
-    // public static void main(String args[]) {
-    // /* Set the Nimbus look and feel */
-    // // <editor-fold defaultstate="collapsed" desc=" Look and feel setting code
-    // // (optional) ">
-    // /*
-    // * If Nimbus (introduced in Java SE 6) is not available, stay with the default
-    // * look and feel.
-    // * For details see
-    // * http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
-    // */
-    // try {
-    // for (javax.swing.UIManager.LookAndFeelInfo info :
-    // javax.swing.UIManager.getInstalledLookAndFeels()) {
-    // if ("Nimbus".equals(info.getName())) {
-    // javax.swing.UIManager.setLookAndFeel(info.getClassName());
-    // break;
-    // }
-    // }
-    // } catch (ClassNotFoundException ex) {
-    // java.util.logging.Logger.getLogger(OltCutoverFormDestino.class.getName())
-    // .log(java.util.logging.Level.SEVERE, null, ex);
-    // } catch (InstantiationException ex) {
-    // java.util.logging.Logger.getLogger(OltCutoverFormDestino.class.getName())
-    // .log(java.util.logging.Level.SEVERE, null, ex);
-    // } catch (IllegalAccessException ex) {
-    // java.util.logging.Logger.getLogger(OltCutoverFormDestino.class.getName())
-    // .log(java.util.logging.Level.SEVERE, null, ex);
-    // } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-    // java.util.logging.Logger.getLogger(OltCutoverFormDestino.class.getName())
-    // .log(java.util.logging.Level.SEVERE, null, ex);
-    // }
-    // // </editor-fold>
-
-    // /* Create and display the form */
-    // java.awt.EventQueue.invokeLater(new Runnable() {
-    // public void run() {
-    // new OltCutoverFormDestino().setVisible(true);
-    // }
-    // });
-    // }
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButtonOK;
 
@@ -142,10 +98,6 @@ public class OltCutoverFormDestino extends javax.swing.JFrame {
             }
             oltsRoot.add(oltNode);
 
-            // UPLINK's
-            // javax.swing.tree.DefaultMutableTreeNode uplinkModel = new
-            // javax.swing.tree.DefaultMutableTreeNode(oltName);
-            // uplinksRoot.add(uplinkModel);
             javax.swing.tree.DefaultMutableTreeNode uplinkModel = new javax.swing.tree.DefaultMutableTreeNode(oltName);
 
             switch (oltName) {
